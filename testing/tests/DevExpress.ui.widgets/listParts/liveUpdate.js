c1fa21eab3adeca2959ae821f0209fd4035e3704
Alexander Komarov <sanrkom@gmail.com>
2018-09-21 11:11:17 +0300

    + some changes

--- testing/tests/DevExpress.ui.widgets/listParts/liveUpdate.js
+++ testing/tests/DevExpress.ui.widgets/listParts/liveUpdate.js
@@ -4,26 +4,25 @@ import { DataSource } from "data/data_source/data_source";
 import "ui/list";
 QUnit.module("live update", {
     beforeEach: function() {
-        var dataSource = new DataSource({
-            load: () => [{ a: "Item 0", id: 0 }, { a: "Item 1", id: 1 }],
-            key: "id"
-        });
+        this.itemRenderedSpy = sinon.spy();
+        this.itemDeletedSpy = sinon.spy();
+        this.createList = () => {
+            var dataSource = new DataSource({
+                load: () => [{ a: "Item 0", id: 0 }, { a: "Item 1", id: 1 }],
+                key: "id"
+            });
 
-        $("#templated-list").dxList({
-            items: dataSource
-        });
-
+            return $("#templated-list").dxList({
+                items: dataSource,
+                onItemRendered: this.itemRenderedSpy,
+                onItemDeleted: this.itemDeletedSpy,
+            }).dxList("instance");
+        };
     }
 }, function() {
     QUnit.test("only updated item is refreshed", function(assert) {
-        var items = [{ a: 0 }, { a: 1 }];
-
-        var $list = $("#templated-list").dxList({
-            items: items
-        });
-
-        assert.equal(list.getFlatIndexByItemElement($item.get(0)), 1, "index correct");
-        assert.equal(list.getFlatIndexByItemElement($item), 1, "index correct");
+        var store = this.createList().getDataSource.store();
+        store.push([{ type: "update", data: { a: "Item 0 Updated", id: 0 }, id: 0 }]);
     });
 });
 */
